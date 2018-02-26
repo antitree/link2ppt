ae9f7b278874f4561f41fce08fa78d86f0a10c7c
antitree <antitree@protonmail.com>
2018-02-26 15:09:50 -0500

    adding new categorizer function

---
+++ categorizer.py
@@ -0,0 +1,30 @@
+from google.cloud import language
+from google.cloud.language import enums
+from google.cloud.language import types
+import six
+import logging
+
+class Categorize:
+	def __init__(self,content):
+		print("Yep")
+		self.content = content
+
+	def classify_text(self):
+	    """Classifies content categories of the provided text."""
+	    text = self.content
+	    client = language.LanguageServiceClient()
+
+	    if isinstance(text, six.binary_type):
+	        text = text.decode('utf-8')
+
+	    document = types.Document(
+	        content=text.encode('utf-8'),
+	        type=enums.Document.Type.PLAIN_TEXT)
+
+	    categories = client.classify_text(document).categories
+
+	    for category in categories:
+	        logging.info(u'=' * 20)
+	        logging.info(u'{:<16}: {}'.format('name', category.name))
+	        logging.info(u'{:<16}: {}'.format('confidence', category.confidence))
+	    return categories
