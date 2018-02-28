b2d1290fb7f611b0d4c634302fc0f1a21bc96a1e
antitree <antitree@protonmail.com>
2018-02-27 20:33:48 -0500

    handling exceptions a bit better

--- categorizer.py
+++ categorizer.py
@@ -5,33 +5,41 @@ import six
 import logging
 
 class Categorize:
-	def __init__(self):
-		"""Classifies content categories of the provided text."""
-		self.client = language.LanguageServiceClient()
+    def __init__(self):
+        """Classifies content categories of the provided text."""
+        self.client = language.LanguageServiceClient()
 
-	def classify_text(self, text):
-	    if isinstance(text, six.binary_type):
-	        text = text.decode('utf-8')
+    def classify_text(self, text):
+        if isinstance(text, six.binary_type):
+            text = text.decode('utf-8')
 
-	    document = types.Document(
-	        content=text.encode('utf-8'),
-	        type=enums.Document.Type.PLAIN_TEXT)
+            if len(text) < 50:
+                categories = [Empty()]
+                catgories[0].name = "WHATEVER"
+                categoies[0].confidence = "0.01"
+                return categories
 
-	    categories = self.client.classify_text(document).categories
-	    if len(categories) == 0:
-	    	class Empty: pass
+        document = types.Document(
+            content=text.encode('utf-8'),
+            type=enums.Document.Type.PLAIN_TEXT)
 
-	    	categories = [Empty()]  ## can i even do this?
-	    	categories[0].name = "GOOGLE FAILED ME"
-	    	categories[0].confidence = "0.99"
+        categories = self.client.classify_text(document).categories
+        if len(categories) == 0:
+            categories = [Empty()]  ## can i even do this?
+            categories[0].name = "GOOGLE FAILED ME"
+            categories[0].confidence = "0.99"
 
-	    	logging.info(u'=' * 20)
-	    	logging.info(text)
+            logging.info(u'=' * 20)
+            logging.info(text)
 
 
-	    for category in categories:
-	        logging.info(u'=' * 20)
-	        logging.info(u'{:<16}: {}'.format('name', category.name))
-	        logging.info(u'{:<16}: {}'.format('confidence', category.confidence))
-	    
-	    return categories
+        for category in categories:
+            logging.info(u'=' * 20)
+            logging.info(u'{:<16}: {}'.format('name', category.name))
+            logging.info(u'{:<16}: {}'.format('confidence', category.confidence))
+        
+        return categories
+
+class Empty:
+    def __init__(self):
+        self.name, self.confidence = "", ""
